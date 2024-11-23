de3a5d1404b8223cbefeb3eb22027b679654945b
Shavko dima <shavkodima10@gmail.com>
2024-11-23 15:21:56 +0300

    update file app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { JobsModule } from './jobs/jobs.module';
 
 @Module({
   imports: [
-    // MongooseModule.forRoot('mongodb://localhost:27017', { dbName: 'jobs_db' }),
+    //MongooseModule.forRoot('mongodb://localhost:27017', { dbName: 'jobs_db' }), localhost
     MongooseModule.forRoot(
       'mongodb://mongo:ayNEnzTUSFGZcdHpsBlKUUbHhEvxmnwA@autorack.proxy.rlwy.net:15781',
     ),
